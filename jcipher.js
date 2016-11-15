3ea00c06aeb124295aac06dadf61afeecbec1c47
jfgrodrigues <jfgrodrigues@users.noreply.github.com>
2016-11-15 21:33:14 -0200

    Algoritmo escrito em javascript

---
+++ jcipher.js
@@ -0,0 +1,49 @@
+var criptografar = function (msgPlana, chave){
+	msgPlana += chave.length;
+	msgNova = inverter(chave)+msgPlana;
+	return cifrar(msgNova, chave);
+}
+
+var inverter = function (stringInicial){
+	var stringInvertida = "";
+	for (i=stringInicial.length; i>=0; i--){
+		stringInvertida += stringInicial.charAt(i);
+	}
+	console.log("stringInvertida: "+stringInvertida);
+	return stringInvertida;
+}
+
+var cifrar = function(mensagem, chave){
+	var msgCifrada = "";
+	for (i=0; i<mensagem.length; i++){
+		msgCifrada += String.fromCharCode(mensagem.charCodeAt(i)+chave.length);
+	}
+	return msgCifrada;
+}
+
+var decifrar = function (mensagemCriptografada, chave){
+	var msgPlana = "";
+	for (i=0; i<mensagemCriptografada.length; i++){
+		msgPlana += String.fromCharCode(mensagemCriptografada.charCodeAt(i)-chave.length);
+	}
+	return msgPlana
+}
+
+var descriptografar = function (mensagem, chave){
+	var msgDecifrada = decifrar(mensagem, chave);
+	var verifChave = "";
+	for (i=0; i<chave.length; i++){
+		verifChave += msgDecifrada.charAt(i);
+	}
+	if (verifChave == inverter(chave)) {
+		var corte;
+		if (chave.length < 10){
+			corte = mensagem.length-1;
+		} else if (chave.length<100){
+			corte = mensagem.length-2;
+		}
+		return msgDecifrada.slice(chave.length, corte);
+	} else { 
+		return "chave de criptografia incompativel";
+	}
+}
